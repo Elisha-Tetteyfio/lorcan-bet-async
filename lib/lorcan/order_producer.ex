24f73312ef03148b5ba4534e57df28c4b4d7a106
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2024-09-15 03:14:45 +0000

    add timer for background job

--- lib/lorcan/order_producer.ex
+++ lib/lorcan/order_producer.ex
@@ -4,12 +4,27 @@ defmodule Lorcan.OrderProducer do
   alias Lorcan.Repo
   use GenStage
 
+  @poll_interval 60_000  # Run every 60 seconds
+
   def start_link(_) do
     GenStage.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
   def init(:ok) do
-    {:producer, []}
+    :timer.send_interval(@poll_interval, :poll)
+    {:producer, %{demand: 0, last_checked: NaiveDateTime.utc_now()}}
+  end
+
+  def handle_info(:poll, state) do
+    # Fetch orders based on last checked time
+    orders = fetch_orders(state.last_checked)
+    new_state = %{state | last_checked: NaiveDateTime.utc_now()}
+    {:noreply, orders, new_state}
+  end
+
+  defp fetch_orders(last_checked) do
+    # Fetch orders created since the last check
+    Repo.all(from o in Order, where: o.status == "pending" and o.inserted_at > ^last_checked)
   end
 
   def handle_demand(demand, state) do
